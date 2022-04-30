6308b1bd5f58db02983ef772f987882145eacc22
avan-pra <avan-pra@users.noreply.github.com>
2022-04-30 22:27:46 +0200

    well the bruteforce did actually work

--- bruteforce.py
+++ bruteforce.py
@@ -4,17 +4,20 @@ import subprocess
 
 c_sol = "Public speaking is very easy.\n1 2 6 24 120 720\n2 b 755\n9\no05km1\n"
 
-
 for i in range(400000, 500000):
-    l = list(str(i))
-    j = 1
-    while True:
-        if (j == 11):
-            break
-        l.insert(j, ' ');
-        j = j + 2
-    print(''.join(l))
-    file = open("./defuse", "w");
-    file.write(c_sol + ''.join(l) + '\n')
-    file.close()
-    subprocess.run(["./bomb", "defuse"])
+	l = list(str(i))
+	j = 1
+	while True:
+		if (j == 11):
+			break
+		l.insert(j, ' ');
+		j = j + 2
+	file = open("./defuse", "w");
+	file.write(c_sol + ''.join(l) + '\n')
+	file.close()
+	try:
+		if 'Congratulations' in subprocess.check_output(["./bomb", "defuse"]).decode('utf-8'):
+			print(''.join(l))
+			exit()
+	except subprocess.CalledProcessError:
+		pass
